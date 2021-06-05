d7fd8c91719643d75a3cf2056d6a91122e84bf71
Ferhat Ozkasgarli <ferhat@dice.fm>
2021-06-05 12:13:12 -0400

    enc creds.json

--- dnsconfig.js
+++ dnsconfig.js
@@ -23,5 +23,6 @@ D("veriga.net",
     MX("@",20,"mx2.zoho.com."),
     MX("@",30,"mx3.zoho.com."),
     TXT("@", "v=spf1 include:zoho.com include:spf.mandrillapp.com ~all"),
-    TXT("@", "zoho-verification=zb09169468.zmverify.zoho.com")
+    TXT("@", "zoho-verification=zb09169468.zmverify.zoho.com"),
+    TXT("zmail._domainkey", "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCXqDRJL8B6Df3pwGNSIvOeLcG8vcQ4Y9zOMTIbrtCIOw8m4C7L4Zt64fFleYFmOqRVoPeHPGLGLQlQtBq7ZLjerIoO3Y7kV4dGb8blUTAMCDEedRJlZ/uwn6Idr+nBx3z33az6G2RQzdjP4K46/v8qSbKLPzeTc1OdshMI8B3q/QIDAQAB")
 )
