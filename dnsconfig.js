a5f7d64519879527fe3799af7e3f83eb81946856
Fehat Ozkasgari <ferhat@dice.fm>
2022-03-02 00:08:46 -0500

    stg.veriga.net

--- dnsconfig.js
+++ dnsconfig.js
@@ -8,23 +8,25 @@ var CF_PROXY_ON = {'cloudflare_proxy': 'on'};       // Proxy enabled.
 
 // k8s clusters
 var K8S0 = "97123e645323ef75300036546ecbafc0-408290970.eu-west-1.elb.amazonaws.com."
-var K8S1 = "178.128.132.16"
+var K8S1 = "k8s1.veriga.net."
 
 /**** veriga.net ****/
 D("veriga.net",
     REG_NONE,
     DnsProvider(CLOUDFLARE),
-    A("k8s1", K8S1, CF_PROXY_ON),
+    A("k8s1", "134.209.128.143", CF_PROXY_ON),
     ALIAS("@", "97123e645323ef75300036546ecbafc0-408290970.eu-west-1.elb.amazonaws.com.", CF_PROXY_ON),
     CNAME("charts", "chartmuseum0.o1eidk0t6b778.eu-west-1.cs.amazonlightsail.com.", CF_PROXY_OFF),
     CNAME("k8s0", K8S0, CF_PROXY_ON),
     CNAME("droneci", K8S0, CF_PROXY_ON),
+    CNAME("droneci1", K8S1, CF_PROXY_ON),
     CNAME("notary", K8S0, CF_PROXY_ON),
     CNAME("tasks", K8S0, CF_PROXY_ON),
     CNAME("youtrack", "veriganet.myjetbrains.com.", CF_PROXY_ON),
     CNAME("registry", K8S0, CF_PROXY_ON),
     CNAME("registry-aws", K8S0, CF_PROXY_ON),
     CNAME("www", K8S0, CF_PROXY_ON),
+    CNAME("stg", K8S1, CF_PROXY_ON),
     CNAME("_79f74ed889024fce4615ae0db3ec6350", "_4cfaaa5018d5ce413be013df88775e01.xrchbtpdjs.acm-validations.aws.", CF_PROXY_OFF),
     CNAME("_98177084447eadfbb6e2e4b9e20fd710.charts.veriga.net.", "_3309afecd601d776b22211545814c238.hcxvpdkkrx.acm-validations.aws.", CF_PROXY_OFF),
     MX("@",10,"mx.zoho.com."),
