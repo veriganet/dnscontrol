71eb0de7ab869e4785d2071ec777638426f58fac
Fehat Ozkasgari <ferhat@dice.fm>
2022-03-06 08:51:20 -0500

    mandrill

--- dnsconfig.js
+++ dnsconfig.js
@@ -21,7 +21,7 @@ D("veriga.net",
     CNAME("droneci", K8S0, CF_PROXY_ON),
     CNAME("droneci1", K8S1, CF_PROXY_ON),
     CNAME("notary", K8S0, CF_PROXY_ON),
-    CNAME("tasks", K8S0, CF_PROXY_ON),
+    CNAME("tasks", K8S1, CF_PROXY_ON),
     CNAME("youtrack", "veriganet.myjetbrains.com.", CF_PROXY_ON),
     CNAME("registry", K8S0, CF_PROXY_ON),
     CNAME("registry-aws", K8S0, CF_PROXY_ON),
@@ -32,12 +32,14 @@ D("veriga.net",
     MX("@",10,"mx.zoho.com."),
     MX("@",20,"mx2.zoho.com."),
     MX("@",30,"mx3.zoho.com."),
-    TXT("@", "v=spf1 include:zoho.com include:spf.mandrillapp.com ~all"),
     TXT("@", "zoho-verification=zb09169468.zmverify.zoho.com"),
     TXT("zmail._domainkey", "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCXqDRJL8B6Df3pwGNSIvOeLcG8vcQ4Y9zOMTIbrtCIOw8m4C7L4Zt64fFleYFmOqRVoPeHPGLGLQlQtBq7ZLjerIoO3Y7kV4dGb8blUTAMCDEedRJlZ/uwn6Idr+nBx3z33az6G2RQzdjP4K46/v8qSbKLPzeTc1OdshMI8B3q/QIDAQAB"),
     // AWS Simple Mail Service
     TXT("_amazonses", "e+jUcAB5gu0vMfK6zkP41og6xh/Kj+wRRfDSxTHobh8="),
     CNAME("lm6kyszxwsadisi4djdcfopgaha4jcaa._domainkey", "lm6kyszxwsadisi4djdcfopgaha4jcaa.dkim.amazonses.com.", CF_PROXY_OFF),
     CNAME("grto6q7zwdkf2ugak4ervn7pysmixfyr._domainkey", "grto6q7zwdkf2ugak4ervn7pysmixfyr.dkim.amazonses.com.", CF_PROXY_OFF),
-    CNAME("ljo6letvym2abbosrbuv5xv32wjspdu5._domainkey", "ljo6letvym2abbosrbuv5xv32wjspdu5.dkim.amazonses.com.", CF_PROXY_OFF)
+    CNAME("ljo6letvym2abbosrbuv5xv32wjspdu5._domainkey", "ljo6letvym2abbosrbuv5xv32wjspdu5.dkim.amazonses.com.", CF_PROXY_OFF),
+    // Mailchimp / Mandrill
+    TXT("@", "v=spf1 include:zoho.com include:spf.mandrillapp.com ~all"),
+    TXT("mandrill._domainkey", "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCrLHiExVd55zd/IQ/J/mRwSRMAocV/hMB3jXwaHH36d9NaVynQFYV8NaWi69c1veUtRzGt7yAioXqLj7Z4TeEUoOLgrKsn8YnckGs9i3B3tVFB+Ch/4mPhXWiNfNdynHWBcPcbJ8kjEQ2U8y78dHZj1YeRXXVvWob2OaKynO8/lQIDAQAB;")
 )
